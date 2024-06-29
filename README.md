3729dae4bcfc0084cac01aaac13c9883d59483cc
beniciont <benicio.neto@outlook.com.br>
2024-06-29 15:38:56 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Pockets Docker Scripts
---
+++ cleanup_docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Parar todos os contêineres
+docker stop $(docker ps -a -q)
+
+# Apagar todos os contêineres
+docker rm $(docker ps -a -q)
+
+# Apagar todas as imagens locais
+docker rmi $(docker images -a -q)
+
+# Remover todos os volumes
+docker volume rm $(docker volume ls -q)
